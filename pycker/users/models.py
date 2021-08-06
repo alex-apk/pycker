e244f2f0f68d5ef8cff3162caae771cdbd0aa7cb
alex-apk <alexey.kovalevsky@miran.ru>
2021-08-06 13:32:43 +0300

    added customfields to users

--- pycker/users/models.py
+++ pycker/users/models.py
@@ -1,6 +1,10 @@
-from django.db import models
+from customfields.models import HasCustomFieldsMixin, CustomFieldsComp
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-    pass
+class User(AbstractUser, HasCustomFieldsMixin):
+    is_catalog = False
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.customfield = CustomFieldsComp(self, 1)
